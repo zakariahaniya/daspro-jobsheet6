57d4c32b23b9db7753350f0b522e0c69b9f7a618
zakariahaniya <zakalvha@gmail.com>
2024-10-03 22:08:39 +0700

    daspro jobsheet 6

--- Pemilihan2Percobaan116.java
+++ Pemilihan2Percobaan116.java
@@ -10,7 +10,12 @@ public class Pemilihan2Percobaan116 {
         System.out.print("Masukkan Tahun : ");
         tahun = sc16.nextInt();
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0)
+            if ((tahun % 100) == 0){
+                if ((tahun % 400 ) == 0) {
+                    System.out.println("Tahun Kabisat");
+                } else 
+                    System.out.println("Bukan Tahun Kabisat");
+            } else
                 System.out.println("Tahun Kabisat");
         } else {
             System.out.println("Bukan Tahun Kabisat");
